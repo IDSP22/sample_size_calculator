f35dd03eab19e080e038d15a904013c83ea50ba4
preranahiriyur <preranah.ds@gmail.com>
2022-04-09 21:56:54 -0400

    minor changes

--- sample_calculator/server.R
+++ sample_calculator/server.R
@@ -12,6 +12,7 @@ library(tidyverse)
 library(sortable)
 library(shinyWidgets)
 library(GGally)
+library(ggplot2)
 
 
 shinyServer(function(input, output, session) {
@@ -37,22 +38,20 @@ shinyServer(function(input, output, session) {
     q2 <- reactive(1-p2())
     k <- 1
     
-    
-    ss <- eventReactive(input$calculate,{
-
-        k <- 1 
-        p <- (p1()+k*p2())/(1+k)
-        q <- (1-p)
-        q1 <- 1-p1()
-        q2 <- 1-p2()
-        print(power())
-        print(qnorm(power()))
-        n1 <- (sqrt(p*q*(1+1/k))*qnorm(1-alpha()/2) + sqrt(p1()*q1 + p2()*q2/k)*qnorm(power()))^2/effect()^2
-        n2 <- k*n1
-        #from towardsdatascience article 
-        round(n1)
+    get_ss <- function(power, alpha, relative_effect, baseline){
+        effect <- relative_effect*baseline 
+        new_prob <- baseline - effect 
+        p <- (baseline+new_prob)/2
+        q <- 1-p
+        q1 <- 1-baseline
+        q2 <- 1-new_prob 
+        n <- (sqrt(p*q*2)*qnorm(1-alpha/2) + sqrt(baseline*q1 + new_prob*q2)*qnorm(power))^2/effect^2
+        round(n)
     }
-    )
+    
+    ss <- reactive({
+        get_ss(power(), alpha(), effectsize(), p1())
+    })
     
     output$samplesize <- renderText({
         req(input$effectsize < 100 & input$effectsize > 0)
@@ -67,7 +66,8 @@ shinyServer(function(input, output, session) {
     output$plot <- renderPlot({
         req(input$plot_y)
         req(input$effectsize < 100 & input$effectsize > 0)
-        sim_x <- runif(20, 0, 2*ss())
+        # sim_x <- runif(20, 0, 2*ss())
+        sim_x <- seq(from=0, to=2*ss(), 20)
         x <- c(ss(), sim_x)
         dat <- data.frame(x)
         if(input$plot_y == 'power') {
@@ -76,21 +76,39 @@ shinyServer(function(input, output, session) {
                 return(power)
             }
             dat <- dat %>% mutate(y = get_power(x))
-            print(dat)
-            plot(x = dat$x, y = dat$y, col=ifelse(x==ss(), "red", "black"),
-                 pch=ifelse(x==ss(), 19, 1), cex=ifelse(x==ss(), 2, 1), ylab = 'power')
+            # print(dat)
+            # plot(x = dat$x, y = dat$y, col=ifelse(x==ss(), "red", "black"),
+            #      pch=ifelse(x==ss(), 19, 1), cex=ifelse(x==ss(), 2, 1), ylab = 'power')
+            ggplot(data=dat, aes(x=x, y=y)) +
+                geom_line()+
+                annotate("point", x = ss(), y = power(), colour = "red", size=3)+
+                xlab("sample size")+ylab("power")
         }
         else if(input$plot_y == 'effect size'){
             get_effect <- function(sampsize) {
                 effect_size <- sqrt((sqrt(p()*q()*(1+1/k))*qnorm(1-alpha()/2) + sqrt(p1()*q1() + p2()*q2()/k)*qnorm(power()))^2/sampsize)/p1()
             }
             dat <- dat %>% mutate(y = get_effect(x))
-            print(dat)
-            plot(x = dat$x, y = dat$y, col=ifelse(x==ss(), "red", "black"),
-                 pch=ifelse(x==ss(), 19, 1), cex=ifelse(x==ss(), 2, 1), ylab = 'effect size')
+            # print(dat)
+            # plot(x = dat$x, y = dat$y, col=ifelse(x==ss(), "red", "black"),
+            #      pch=ifelse(x==ss(), 19, 1), cex=ifelse(x==ss(), 2, 1), ylab = 'effect size')
+            ggplot(data=dat, aes(x=x, y=y)) +
+                geom_line()+
+                annotate("point", x = ss(), y = effectsize(), colour = "red", size=3)+
+                xlab("sample size")+ylab("effect size")
         }
     })
     
 })
+
+
+#Things to do 
+
+# - validate that baseline probability is not less than 0 or more than 100 
+# - add explanation below power and effectsize plot 
+# - clean up plot 
+# - when user hits calculate - freeze x axis of all plots and any changes in inputs should only change plot interior 
+# when user hits calculate - freeze MDE and Baseline - calculate sample size for all
+# 
 
 
--- sample_calculator/ui.R
+++ sample_calculator/ui.R
@@ -14,15 +14,30 @@ library(shinythemes)
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
     
-    navbarPage(title='A/B Testing Toolkit',
+    navbarPage(title='Sample Size Calculator',
                      theme = shinytheme("sandstone"),
+               tabPanel(title="Introduction", 
+                        wellPanel(
+                          h3("Some heading"),
+                          h6("Hypothesis testing allows us to compare statistics between groups of data and determine if there is a significant difference or not. 
+                             A properly designed hypothesis test has many components including the desired significance level, power, 
+                             minimum detectable effect size and baseline probability. 
+                             [Define these here]
+                             Once we have decided on a hypothesis test and considered the above components, we can calculate the minimum sample 
+                             size needed to achieve the desired power and significance level. Proper sample size calculation is therefore a critical step in the 
+                             process of running a hypothesis test. 
+                            "), 
+                          br(), 
+                          h6("Let us consider the example of a simple hypothesis test ....."),
+                          br(),
+                          h6("In this app, you can explore how the minimum sample size required changes with the other 4 components discussed above.")
+                        )),
                      tabPanel(title='Calculate Sample Size',
                               sidebarLayout(
                                   sidebarPanel(
-                                      sliderInput(inputId = "baseline",
-                                                  label = "Baseline Probability", 
-                                                  value = 10,
-                                                  min = 0, max = 100),
+                                    numericInput(inputId = "baseline", 
+                                                 label = "Baseline Probability", 
+                                                 value = 10, min = 0, max = 100),
                                       br(),
                                       numericInput(inputId = "effectsize", 
                                                    label = "Minimum Detectable Effect Size", 
@@ -48,14 +63,18 @@ shinyUI(fluidPage(
                                   mainPanel(
                                       fluidRow(
                                           sidebarPanel(width = 12,
-                                                       h3("Sample Size:"),
-                                                       h2(align ="center",style = "font-size: 400%; letter-spacing: 3px;", textOutput(outputId = "samplesize"))),
+                                                       h4("Estimated Sample Size:"),
+                                                       h4(align ="center",style = "font-size: 400%; letter-spacing: 3px;", textOutput(outputId = "samplesize")))),
+                                      
+                                      fluidRow(
                                           #dropdown menu for variables
+                                        sidebarPanel(width = 12,
                                           selectInput(inputId = 'plot_y', 
                                                       label = 'Choose a variable to plot against sample size:',
                                                       choices = c( 'power', 'effect size')),
                                           plotOutput(outputId = 'plot')
                                           )
+                                      )
                                   )
                               )
                      )
