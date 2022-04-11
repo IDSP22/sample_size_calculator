8d7a2d019abb92630abf50b8aea7bda437c089fe
preranahiriyur <preranah.ds@gmail.com>
2022-04-11 13:23:24 -0400

    added intro

--- sample_calculator/ui.R
+++ sample_calculator/ui.R
@@ -19,22 +19,18 @@ shinyUI(fluidPage(
                         wellPanel(
                           h3("Introduction"),
                           br(),
-                          h5("Hypothesis testing allows us to compare statistics between groups of data and determine if there is a significant difference or not. 
+                          HTML(paste0("Hypothesis testing allows us to compare statistics between groups of data and determine if there is a significant difference or not. 
                               A crucial step in the process of hypothesis testing is sample size calculation. The size of the sample on which the test is conducted affects the power and significance level of the results. 
-                            "), 
-                          br(), 
-                          h5("Let's say we want to conduct a hypothesis test to see if ...  
-                              We know that the current ... rate is .. This is the Baseline Probability. Let's say we only care about a difference in .. of ..% or more. This is the Relative Minimum Detetctable Effect Size. This value 
-                              is decided based on context such that any smaller effect is considered negligible. Let's assume we also want a confidence level of 95% and a power of 80% on this test since this is conventional. 
+                            ", "<br></br>", "Let's say we're evaluating the effectiveness of a new email marketing campaign for an e-commerce company. We want to conduct a hypothesis test to see if the click-through-rate (CTR) on a new version of an email advertisement is higher than the older version. 
+                              We know that the current CTR is 10%. This is called the",  "<b>", " Baseline Probability", "</b>", ". Let's say we only care about a relative difference of 10% or more in CTR, i.e. any absolute change of 1 percentage point or higher is worth looking into. 
+                              This is the ", "<b>", "Relative Minimum Detetctable Effect Size", "</b>", ". This value 
+                              is decided based on context such that any smaller effect is considered negligible. Let's assume we also want a ", "<b>", "confidence level of 95%", "</b>",  " and ", "<b>", "power of 80%", "</b>", " on this test since this is conventional. 
                               Now we need to know how many participants to include in this study so that we achieve the desired power and detect a significant difference. 
-                              This is where a tool to calculate minimum required sample size comes in handy. "),
-                          br(),
-                          h5("Use the next tab to try out these calculations yourself! First, think of a hypothesis test you'd like to conduct. Then, specify a baseline probability and minimum detectable effect size. 
-                              Use the sliders to set the desired power and confidence level of your test and calculate the estimated sample size."), 
-                          br(), 
-                          h5("Sometimes, the estimated sample size is too large or not feasible due to resource constraints. In such situations, it is good to know how you can change the required sample size by altering other parameters. 
+                              This is where a tool to calculate minimum required sample size comes in handy.", "<br></br>", "Use the next tab to try out these calculations yourself! First, think of a hypothesis test you'd like to conduct. Then, specify a baseline probability and minimum detectable effect size. 
+                              Use the sliders to set the desired power and confidence level of your test and calculate the estimated sample size.", 
+                                      "<br></br>", "Sometimes, the estimated sample size is too large or not feasible due to resource constraints. In such situations, it is good to know how you can change the required sample size by altering other parameters. 
                               For example, maybe you can increase the minimum detectable effect to be able to work with smaller sample? Or maybe you wouldn't mind reducing the power of the test slightly?
-                              You can use the plots generated to understand the relationship between sample size and other parameters, and make such decisions. ")
+                              You can use the plots generated to understand the relationship between sample size and other parameters, and make such decisions. ")),
                         )),
                      tabPanel(title='Calculate Sample Size',
                               sidebarLayout(
